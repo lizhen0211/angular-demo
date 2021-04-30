bf69a3090cdc810cb7b18b0f9979f07ddfcb6188
lizhen <lizhen@zhumengyuan.com>
2021-04-30 10:17:25 +0800

    echart-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {EchartDemoComponent} from './echart-demo/echart-demo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'echart-demo', pathMatch: 'full'},
+  {
+    path: 'echart-demo', component: EchartDemoComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/echart-demo/echart-demo.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-echart-demo',
+  templateUrl: './echart-demo.component.html',
+  styleUrls: ['./echart-demo.component.less']
+})
+export class EchartDemoComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
